43123fbba31b9f6a15dae8fd16f6dc72cf7fca27
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2023-07-17 23:38:01 +0530

    Refactored the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,118 +1,63 @@
 import { useState } from "react";
 import "./App.css";
-import ReactModal from "react-modal";
+import Card from "./components/Card";
+import CustomModal from "./components/CustomModal";
+
+interface Todo {
+  task: string;
+  priority: string;
+}
 
 function App() {
   const [modalOpen, setModalOpen] = useState(false);
+  const [modalType, setModalType] = useState("Add Task");
+  const [deleteIdx, setDeleteIdx] = useState<number>(0);
   const [currTask, setCurrTask] = useState("");
   const [taskPriority, setTaskPriority] = useState("");
-  const [savedTodo, setSavedTodo] = useState([]);
+  const [savedTodo, setSavedTodo] = useState<Array<Todo>>([]);
   const priorityButtons = ["High", "Medium", "Low"];
-
-  function handleAfterModalClose(): void {
-    setModalOpen(false);
-  }
-
-  function handleModalRequestClose(): void {
-    setModalOpen(false);
-  }
 
   return (
     <>
       <header>
         <h1>Task List</h1>
-        <button onClick={() => setModalOpen(true)}>Add Task</button>
+        <button
+          onClick={() => {
+            setCurrTask("");
+            setTaskPriority("");
+            setModalType("Add Task");
+            setModalOpen(true);
+          }}
+        >
+          Add Task
+        </button>
       </header>
       <main>
-        <ReactModal
-          isOpen={modalOpen}
-          closeTimeoutMS={0}
-          contentLabel={"Add Task"}
-          shouldCloseOnOverlayClick={true}
-          shouldCloseOnEsc={true}
-          shouldReturnFocusAfterClose={true}
-          onAfterClose={handleAfterModalClose}
-          onRequestClose={handleModalRequestClose}
-        >
-          <div>
-            <div>
-              <h2>Add Task</h2>
-            </div>
-            <div>
-              <p>Task</p>
-              <input
-                type="text"
-                name="task"
-                id="task"
-                value={currTask}
-                onChange={(e) => setCurrTask(e.target.value)}
-                required
-              />
-            </div>
-            <div>
-              <p>Priority</p>
-              <div>
-                {priorityButtons.map((val, idx) => (
-                  <button
-                    key={idx}
-                    onClick={() => {
-                      setTaskPriority(val);
-                    }}
-                  >
-                    {val}
-                  </button>
-                ))}
-              </div>
-            </div>
-            <button
-              type="submit"
-              onClick={() => {
-                const newTodo = { task: currTask, priority: taskPriority };
-                savedTodo.unshift(newTodo);
-                setSavedTodo([...savedTodo]);
-                setModalOpen(false);
-              }}
-              disabled={
-                currTask.trim().length > 0 && taskPriority.trim().length > 0
-                  ? false
-                  : true
-              }
-            >
-              Add
-            </button>
-          </div>
-        </ReactModal>
+        <CustomModal
+          modalType={modalType}
+          modalOpen={modalOpen}
+          setModalOpen={setModalOpen}
+          currTask={currTask}
+          setCurrTask={setCurrTask}
+          taskPriority={taskPriority}
+          setTaskPriority={setTaskPriority}
+          priorityButtons={priorityButtons}
+          savedTodo={savedTodo}
+          setSavedTodo={setSavedTodo}
+          deleteIdx={deleteIdx}
+        />
         <div>
           {savedTodo.map((val, idx) => (
-            <div key={idx}>
-              <div>
-                <p>Task</p>
-                <p>{val["task"]}</p>
-              </div>
-              <div>
-                <p>Priority</p>
-                <p>{val["priority"]}</p>
-              </div>
-              <p
-                onClick={() => {
-                  setCurrTask(val["task"]);
-                  setTaskPriority(val["priority"]);
-                  setModalOpen(true);
-                }}
-              >
-                Edit
-              </p>
-              <p
-                onClick={() => {
-                  savedTodo.splice(idx, 1);
-                  setCurrTask("");
-                  setTaskPriority("");
-                  setSavedTodo([...savedTodo]);
-                }}
-              >
-                Delete
-              </p>
-            </div>
+            <Card
+              key={idx}
+              val={val}
+              idx={idx}
+              setCurrTask={setCurrTask}
+              setDeleteIdx={setDeleteIdx}
+              setModalOpen={setModalOpen}
+              setModalType={setModalType}
+              setTaskPriority={setTaskPriority}
+            />
           ))}
         </div>
       </main>
